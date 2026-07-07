0bb79f6691a9bcded752a156d748db28d92991f7
Harsh Vardhan Chaudhary <hvc7667867051@gmail.com>
2026-07-07 14:57:50 +0530

    Fix uploads directory creation

--- Server/middleware/uploadMiddleware.js
+++ Server/middleware/uploadMiddleware.js
@@ -1,16 +1,23 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = "uploads";
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
     cb(
       null,
-      Date.now() +
-      path.extname(file.originalname)
+      Date.now() + path.extname(file.originalname)
     );
   },
 });
